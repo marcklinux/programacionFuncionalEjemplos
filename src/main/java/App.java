72d52d10228115d547764d2cd2717dab19f1947d
marcklinux@gmail.com <marcklinux@gmail.com>
2024-10-07 12:12:44 -0600

    metodo para generar el cuadrado de un numero dentro de una lista de numeros

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App {
+
+    private void m1SquaredNumbers(List<Integer> numbers){
+        List<Integer> Squarednumbers = numbers.stream().map(n -> n* n).collect(Collectors.toList());
+        Squarednumbers.forEach(System.out::println);
+    }
+
+
+
+    public static void main(String[] args) {
+
+        App app = new App();
+        //app.m1SquaredNumbers(Arrays.asList(1,2,3,4,5));
+
+    }
 }
